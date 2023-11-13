3052713733117c1a2912b785f857484c12420549
Ben Choat <ben.choat@gmail.com>
2023-11-13 12:58:11 -0800

    updated build script

--- build_ngen.sh
+++ build_ngen.sh
@@ -8,21 +8,21 @@
 # directory for the build as well as for labeling a folder where the build and
 # test logs will be saved.
 
-# options should be edited around line 120, where cmake is used to build ngen.
+
+#######################
+# inputs
+#######################
 
 
 # provide name for folder that will hold this ngen version
 # NOTE: this folder is defined relative to the directory in which this file is located/executed.
-folder_name="ngen_20231024"
+folder_name="ngen_20231101"
 
 ################
 # If want to build ngen-cal you need to include a line in the requirements.txt file
-# Build ngen-cal?
-# true of false?
-# build_ngen-cal=true
 
 # string defining requirements.txt file to use when building venv environment
-# rqr_in should be defines relative to the directory in which this script is being execute.
+# rqr_in should be defined relative to the directory in which this script is being execute.
 # the script will copy it to $folder_name_ngen
 rqr_in="requirements_20231024.txt"
 
@@ -30,14 +30,9 @@ rqr_in="requirements_20231024.txt"
 # directory holding or where to place boost
 boostDir=$PWD
 
-
-
-
-
-
-
-
 #########################################################
+
+
 
 # run deactivate and conda deactivate to ensure using clean slate for python environment
 deactivate || true
@@ -50,9 +45,12 @@ baseDir=$PWD
 
 #########
 # update folder_name_ngen
-mkdir "logs_${folder_name}"
 folder_name_ngen="${folder_name}/ngen"
-# define python location
+
+# make folder to store logs in 
+mkdir "logs_${folder_name}"
+
+# define Numpyn location ... I DO NOT THINK THIS IS NECESSARY?
 # export Python_NumPy_INCLUDE_DIRS=/home/bchoat/Projects/NWM_NGEN/${folder_name_ngen}/.venv/lib/python3.10/site-packages
 # export Python_NumPy_INCLUDE_DIR=/home/bchoat/Projects/NWM_NGEN/${folder_name_ngen}/.venv/lib/python3.10/site-packages
 
@@ -62,11 +60,11 @@ folder_name_ngen="${folder_name}/ngen"
 	# install libraries
 	echo -e "\n\ninstalling libraries into bash\n\n"
 	sudo apt-get update &&
-		sudo apt install cmake &&
+		sudo apt-get install cmake &&
 		sudo apt-get install g++ &&
-		sudo apt install gfortran &&
+		sudo apt-get install gfortran &&
 		sudo apt-get install python3-dev &&
-		sudo apt install python3.10 &&
+		sudo apt-get install python3.10 &&
 		sudo apt-get install libudunits2-dev &&
 		sudo apt-get install libnetcdf-dev libnetcdff-dev &&
 		sudo apt-get install libnetcdf-c++4-1 libnetcdf-c++4-dev ||
@@ -80,19 +78,27 @@ folder_name_ngen="${folder_name}/ngen"
 		# following line should update all submodules
 		git submodule update --init --recursive ||
 		exit 1
-	# try updating t-route since get message that I'm using a deprecated version
-# 	git submodule update --init --recursive --remote -- extern/t-route ||
-# 		exit 1
-
 
 
 	echo -e "\n\nbuilding ngen extern libraries\n\n"
 
 	echo -e "\nBuild CFE\n"
 	git submodule update --remote extern/cfe/cfe &&
-	cmake -B extern/cfe/cmake_build -S extern/cfe/cfe -DBASE=OFF -DFORCING=OFF \
-		-DFORCINGPET=OFF -DAETROOTZONE=OFF -DNGEN=ON &&
+ 	cmake -B extern/cfe/cmake_build -S extern/cfe/cfe -DNGEN=ON && # -DBASE=OFF -DFORCING=OFF \
+# 		-DFORCINGPET=OFF -DAETROOTZONE=OFF -DNGEN=ON &&
 		make -C extern/cfe/cmake_build ||
+
+	# UPDATING TO INSTRUCTIONS FOUND HERE: https://github.com/NOAA-OWP/cfe/tree/946a0e8b37d858c168bb4e492d113628ab61be0c
+# 	cd extern/cfe
+#	git clone https://github.com/NOAA-OWP/cfe.git
+#	cd cfe
+#	git checkout ajk/sft_aet_giuh_merge
+#	git clone https://github.com/NOAA-OWP/SoilMoistureProfiles.git smc_coupler # (needed if AETROOTZONE=ON)
+#	mkdir build && cd build &&
+ #	cmake ../ -DBASE=ON &&  # -DNGEN=ON &&
+# 	make &&
+	cd "${baseDir}/${folder_name_ngen}" ||
+
 		exit 1
 
 
@@ -104,7 +110,8 @@ folder_name_ngen="${folder_name}/ngen"
 		git fetch topmodelDev &&
 		git checkout -b topmodelCal topmodelDev/remove-topmodel-output-fromNgen &&
 
-	cd ../../../
+	cd ../../../ ||
+		exit 1
 
 	cmake -B extern/topmodel/cmake_build -S extern/topmodel &&
 		make -C extern/topmodel/cmake_build ||
@@ -114,21 +121,39 @@ folder_name_ngen="${folder_name}/ngen"
 
 	cmake -B extern/evapotranspiration/cmake_build -S \
 			extern/evapotranspiration/evapotranspiration &&
-		make -C extern/evapotranspiration/cmake_build petbmi -j2 &&
+		make -C extern/evapotranspiration/cmake_build || # petbmi -j2 &&, this is not included here: https://github.com/NOAA-OWP/SoilFreezeThaw/blob/master/INSTALL.md
+		exit 1
+
 	
 	echo -e "\nBuild SLoTH\n"
+	cd extern/sloth && git checkout latest # as shown here: https://github.com/NOAA-OWP/SoilFreezeThaw/blob/master/INSTALL.md
+	git -submodule update --init --recursive
+	# editing to match instructions at https://github.com/NOAA-OWP/SLoTH/blob/master/INSTALL.md
+	cd ../../
 	cmake -B extern/sloth/cmake_build -S extern/sloth &&
+# 		cmake --build cmake_build --target all &&
+#	cmake -B extern/sloth/cmake_build -S extern/sloth &&
 		make -C extern/sloth/cmake_build ||
+
+#	cd ../../ ||
+		exit 1	
+
+	# SoilFreezeThaw
+	echo -e "\nbuild SoilFreezeThaw\n"
+	git submodule update --remote extern/SoilFreezeThaw/SoilFreezeThaw &&
+		cmake -B extern/SoilFreezeThaw/cmake_build -S extern/SoilFreezeThaw/SoilFreezeThaw/ -DNGEN=ON &&
+		make -C extern/SoilFreezeThaw/cmake_build ||
 		exit 1
-		
+
 	
-	# compile noah-owp and iso_c_fortran - add path to NetCDF libraries
+	# compile 
 	echo -e "\nbuild noah-owp and adding netcfd libraries\n"
 	cmake -B extern/noah-owp-modular/cmake_build -S extern/noah-owp-modular \
 		-DnetCDF_INCLUDE_DIR=/usr/include/ -DnetCDF_MOD_PATH=/usr/include/ \
 		-DnetCDF_FORTRAN_LIB=/usr/lib/x86_64_linux_gnu/libnetcdff.so &&
 		make -C extern/noah-owp-modular/cmake_build ||
 		exit 1
+
 
 	echo -e "build iso_c_fortran_bmi" 
 	cmake -B extern/iso_c_fortran_bmi/cmake_build -S extern/iso_c_fortran_bmi &&
@@ -141,10 +166,12 @@ folder_name_ngen="${folder_name}/ngen"
 		make -C extern/test_bmi_c/cmake_build ||
 		exit 1
 	
+
 	echo -e "\nbuild test_bmi_fortran\n"
 	cmake -B extern/test_bmi_fortran/cmake_build -S extern/test_bmi_fortran &&
 		make -C extern/test_bmi_fortran/cmake_build ||
 		exit 1
+
 
 	# get boost library if not already stored in project folder
 	echo -e "\n\ninstalling boost if needed\n\n"
@@ -160,7 +187,7 @@ folder_name_ngen="${folder_name}/ngen"
 	# create python virtual environment
 	echo -e "\n\ninstalling python libraries w/venv\n\n"
 	# install python-venv to get ensurepip
-	sudo apt install python3.10-venv
+	sudo apt-get install python3.10-venv
 	# copy requirments file to $folder_name_ngen
 	sudo cp $baseDir/$rqr_in .
 	mkdir venv &&
@@ -185,7 +212,7 @@ folder_name_ngen="${folder_name}/ngen"
         # Working with newest t-route
         ################
 
-#       # first, remove default t-route folder that comes with ngen, it is old
+       # first, remove default t-route folder that comes with ngen, it is old
 #       cd extern
 ##      sudo rm -rf t-route
 #       mv t-route t-route_ORG
@@ -198,10 +225,17 @@ folder_name_ngen="${folder_name}/ngen"
 #       ./compiler.sh
 
 	###############
-	# Working with default t-route (version that comes with ngen repo
+	# Working with old t-route (version that was already on this machine)
 	##############
+	# try using old t-route
+	cd $baseDir/$folder_name_ngen/extern/
+	mv t-route t-route_default # stash 'default' folder that comes with ngen
+	ln -s /home/west/git_repositories/ngen_11112022/ngen/extern/t-route
+	# install additional python dependencies (internal to t-route)
+	pip install -e t-route/src/ngen_routing
+	pip install -e t-route/src/nwm_routing
 	cd $baseDir/$folder_name_ngen/extern/t-route/src/python_routing_v02
-	echo -e $PWD
+#	echo -e $PWD
 	./compiler.sh
 
 
@@ -210,13 +244,13 @@ folder_name_ngen="${folder_name}/ngen"
 	echo -e "\n\nbuilding ngen\n\n"
 	cmake -DCMAKE_BUILD_TYPE=Debug -B cmake_build -S . \
 		-DBoost_INCLUDE_DIR=$boostDir/boost_1_77_0 \
-		-DNGEN_ACTIVATE_PYTHON:BOOL=ON \
-		-DNGEN_ACTIVATE_ROUTING:BOOL=ON \
-		-DBMI_C_LIB_ACTIVE:BOOL=ON \
-		-DBMI_FORTRAN_ACTIVE:BOOL=ON \
-		-DNETCDF_ACTIVE:BOOL=ON &&
-		# -DMPI_ACTIVE:=BOOL=ON && \
-		# -DLSTM_TORCH_LIB_ACTIVE:=ON && \
+		-DNGEN_ACTIVATE_PYTHON=ON \
+		-DNGEN_ACTIVATE_ROUTING=ON \
+		-DBMI_C_LIB_ACTIVE=ON \
+		-DBMI_FORTRAN_ACTIVE=ON \
+		-DNETCDF_ACTIVE=ON &&
+		# -DMPI_ACTIVE=ON && \
+		# -DLSTM_TORCH_LIB_ACTIVE=ON && \
 		make -j 8 -C cmake_build ||
 		exit 1
 
@@ -225,14 +259,6 @@ folder_name_ngen="${folder_name}/ngen"
 
 
 	###################	
-
-	# copy build_ngen.sh and requirements.txt files to build folder
-#	cd $baseDir/$folder_name
-#	echo -e "${baseDir}/${folder_name_ngen}"
-	echo -e "PWD: $PWD\n"
-	sudo mkdir "${baseDir}/${folder_name}/BuildScripts"
-	sudo mv "${baseDir}/${folder_name_ngen}/${rqr_in}" "${baseDir}/${folder_name}/BuildScripts"
-	sudo cp "${baseDir}/build_ngen.sh" "${baseDir}/${folder_name}/BuildScripts"
 
 	cd $baseDir
 
@@ -244,14 +270,30 @@ folder_name_ngen="${folder_name}/ngen"
 	cd "${baseDir}/${folder_name_ngen}"
 	echo -e "PWD: $PWD\n"
 
-	source "${folder_name_ngen}/venv/bin/activate"
-
+	source ./venv/bin/activate
 
 #	./cmake_build/test/test_all &&
 	./cmake_build/test/test_bmi_c &&
 	./cmake_build/test/test_bmi_fortran &&
 	./cmake_build/test/test_bmi_python ||
 	exit 1
+
+	###############
+
+	# cfe
+#	echo -d "\ntesting cfe\n"
+#	cd "${basedir}/${folder_name_ngen}/extern/cfe/cfe"
+#	mkdir build && cd build
+#	cmake .. -DUNITTEST=ON &&
+#	make && cd .. &&
+#	cd test && ./run_unittest.sh ||
+#	exit 1
+
+	# cfe NEWEST (Test is build when cfe is build)
+#	cd "${baseDir}/${folder_name_ngen}/extern/cfe/cfe/"
+#	./run_cfe.sh
+
+	#############################
 
 	# t-route
 #	echo -e "\n\ntesting t-route\n\n"
@@ -260,17 +302,18 @@ folder_name_ngen="${folder_name}/ngen"
 #	python -m nwm_routing -f test_AnA.yaml
 
 
-	# CODE FROM LUCIANA - OLD
-	# t-route unit-test (changing syntax for consistency
-#	echo -e "\n\nbuilding and testing test_routing_pybind\n\n"
-	# make -C cmake_build test_routing_pybind &&
-#	cmake --build cmake_build --target test_routing_pybind &&
-# 	cmake -B cmake_build -S 
-#	./cmake_build/test/test_routing_pybind ||
-#		exit 1
-
 } | tee -a "logs_${folder_name}/test_log.txt" 2>&1
 
+# copy build_ngen.sh and requirements.txt files to build folder
+#	cd $baseDir/$folder_name
+#	echo -e "${baseDir}/${folder_name_ngen}"
+echo -e "PWD: $PWD\n"
+mkdir "${baseDir}/${folder_name}/BuildScripts" &&
+	mv "${baseDir}/${folder_name_ngen}/${rqr_in}" "${baseDir}/${folder_name}/BuildScripts" &&
+	cp "${baseDir}/build_ngen.sh" "${baseDir}/${folder_name}/BuildScripts" ||
+	exit 1
+
 # move logs to new build
-mv "logs_${folder_name}" $folder_name
+mv "logs_${folder_name}" $folder_name ||
+	exit 1
 
